b24ecbc24a39784091bd892e7b8d9ba25d44fb72
Niloofar Gharavi <niloofar_nemone@yahoo.com>
2018-10-27 21:08:43 -0700

    fixed more bugs

--- eventry-app/components/FBLoginButton.js
+++ eventry-app/components/FBLoginButton.js
@@ -1,9 +1,9 @@
-import PropTypes from 'prop-types';
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-//import styles from '../Style/Style.js';
-import { Ionicons } from '@expo/vector-icons';
-import { LoginButton } from 'react-native-fbsdk';
+import PropTypes from "prop-types";
+import React, {Component} from "react";
+import {Text, View} from "react-native";
+//import styles from "../Style/Style.js";
+import { Ionicons } from "@expo/vector-icons";
+import { LoginButton } from "react-native-fbsdk";
 
 export default class FBLoginButton extends Component {
   render() {
